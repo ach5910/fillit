794190da004811a205d7d1ae7540bf3e1a1a77ab
Aaron Hunt <ahunt@e1z1r5p40.42.us.org>
2016-10-05 18:18:34 -0700

    seg fault

--- main.c
+++ main.c
@@ -55,7 +55,7 @@ t_vec	*new_vector(int x, int y)
 	return (vector);
 }
 
-void	get_limits(char *str, t_vec min, t_vec max)
+void	get_limits(char *str, t_vec *min, t_vec *max)
 {
 	int i;
 
@@ -88,8 +88,10 @@ t_tet	*get_tetri(char *str, char c)
 	min = new_vector(3, 3);
 	max = new_vector(0, 0);
 
+	ft_putstr(str);
+
 	get_limits(str, min, max);
-	pos = ft_memalloc(sizeof(char *) * max->y - min->y + 1);
+	pos = ft_memalloc(sizeof(char *) * (max->y - min->y + 1));
 	i = 0;
 	while (i < max->y - min->y + 1)
 	{
@@ -142,9 +144,9 @@ int		is_valid_input(char *str, int size_read)
 	{
 		if (i % 5 < 4)
 		{
-			if (str[i] != '.' || str[i] != '#')
+			if (str[i] != '.' && str[i] != '#')
 				return (0);
-			if (str[i] == "#" && ++blocks > 4)
+			if (str[i] == '#' && ++blocks > 4)
 				return (0);
 		}
 		else if (str[i] != '\n')
@@ -158,24 +160,31 @@ int		is_valid_input(char *str, int size_read)
 
 t_list	*read_source(int fd)
 {
-	char	buf[21];
+	char	*buf;
 	int		size_read;
 	t_tet	*tetri;
 	t_list	*list;
 	char	c;
+	int errnum;
 
+	buf = ft_strnew(21);
+	list = NULL;
+	c = 'A';
 	while ((size_read = read(fd, buf, 21)) >= 20)
 	{
-		if (!is_valid_input(buf, size_read) || (tetri = get_tetri(buf, c) == NULL))
+		if (!(errnum = is_valid_input(buf, size_read)) || (tetri = get_tetri(buf, c)) == NULL)
 		{
-			ft_memdel((void **)&buf);
+			//ft_memdel((void **)&buf);
 			//free list
+			ft_putnbr(errnum);
+			ft_putendl("\nFailed if");
 			return (NULL);
 		}
+		c++;
 		ft_lstadd(&list, ft_lstnew(tetri, sizeof(t_tet)));
 		//free tetris ??
 	}
-	ft_memdel((void **)&buf);
+	//ft_memdel((void **)&buf);
 	if (size_read != 0)
 	{
 		//free list
@@ -200,5 +209,7 @@ int		main(int argc, char **argv)
 		ft_putendl("File wouldn't open");
 		return (0);
 	}
+	ft_putendl("going to read source");
 	list = read_source(fd);
+	print_list(&list);
 }
